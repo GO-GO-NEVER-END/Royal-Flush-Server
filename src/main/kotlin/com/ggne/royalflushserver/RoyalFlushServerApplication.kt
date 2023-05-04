c0becec53150db12241b848730892b96dd8ff488
bngsh <hee132@korea.ac.kr>
2023-05-04 20:41:55 +0900

    init: 프로젝트 생성
    Co-authored-by: swjw13 <swjw13@gmail.com>

---
+++ src/main/kotlin/com/ggne/royalflushserver/RoyalFlushServerApplication.kt
@@ -0,0 +1,11 @@
+package com.ggne.royalflushserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RoyalFlushServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<RoyalFlushServerApplication>(*args)
+}
