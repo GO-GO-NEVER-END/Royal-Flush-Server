3895ec847d56ac422e999903e847c8fcacaf43f6
bngsh <hee132@korea.ac.kr>
2023-05-04 20:58:44 +0900

    feat: 디렉토리 설정
    Co-authored-by: swjw13 <swjw13@gmail.com>

---
+++ src/main/kotlin/com/ggne/royalflushserver/teatime/presentation/TeatimeController.java
@@ -0,0 +1,9 @@
+package com.ggne.royalflushserver.teatime.presentation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/teatime")
+@RestController
+public class TeatimeController {
+}
---
+++ src/main/kotlin/com/ggne/royalflushserver/toilet/presentation/ToiletController.kt
@@ -0,0 +1,9 @@
+package com.ggne.royalflushserver.toilet.presentation
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/api/more")
+@RestController
+class ToiletController {
+}
