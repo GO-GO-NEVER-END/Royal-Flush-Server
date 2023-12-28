ffd5e21dea8e1ecf42426eb35fdcf19cbb6cb7a8
swjw13 <swjw13@gmail.com>
2023-12-28 16:32:14 +0900

    feat: 유저 정보 조회 테스트 코드 작성

---
+++ src/test/kotlin/com/ggne/royalflushserver/user/adapter/out/persistence/UserPersistenceAdapterTest.kt
@@ -0,0 +1,41 @@
+package com.ggne.royalflushserver.user.adapter.out.persistence
+
+import com.ggne.royalflushserver.user.adapter.out.persistence.repository.UserRepository
+import com.ggne.royalflushserver.user.domain.model.User
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UserPersistenceAdapterTest {
+
+    @Autowired
+    private lateinit var userRepository: UserRepository
+
+    @Autowired
+    private lateinit var userPersistenceAdapter: UserPersistenceAdapter
+
+    private lateinit var user: User
+
+    @BeforeEach
+    fun setUp() {
+        user = User(
+            id = "id",
+            password = "password",
+            name = "테스트",
+            coin = 0,
+            isSsamulie = false
+        )
+    }
+
+    @Test
+    fun `유저 insert 테스트`() {
+        userPersistenceAdapter.saveUser(user)
+
+        val result = userPersistenceAdapter.findUser("id")
+        assertThat(result).isNotNull
+    }
+
+}
